099f9ab313ae3b8d66463eabe815ebd2081138f9
DongwooSuh <kehops@kaist.ac.kr>
2016-04-03 00:35:07 +0900

    [add] added affine transformation decomposition functions

--- common/affine.hpp
+++ common/affine.hpp
@@ -3,6 +3,7 @@
 #include <glm/glm.hpp>
 #include <glm/ext.hpp>
 #include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/matrix_access.hpp>
 
 /*
  * An affine matrix A can be factored as A = TL. You need to fill up two function named 'linearFact' and 'transFact'
@@ -13,16 +14,21 @@
 // output: L (4 x 4 matrix)
 glm::mat4 linearFact(glm::mat4 A)
 {
-    glm::mat4 L;
+    glm::mat4 L = glm::mat4(1.0f);
+    for (int i = 0; i < 3; i++) {
+        glm::column(L, i, glm::column(A, i));
+    }
+    glm::row(L, 3, glm::row(A, 3));
     return L;
 }
 
 // TODO: Fill up transFact function
 // input: A (4 x 4 matrix)
 // output: T (4 x 4 matrix)
-glm::mat4 transFact(glm::mat4 M)
+glm::mat4 transFact(glm::mat4 A)
 {
     glm::mat4 T = glm::mat4(1.0f);
+    glm::column(T, 3, glm::column(A, 3));
     return T;
 }
 #endif
